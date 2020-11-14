e4afb407ab0553fcfad3f46024f1545fc61c146d
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2020-11-14 14:14:43 -0800

    Create product-of-array-except-self.py

---
+++ 75/product-of-array-except-self.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod = [1] * len(nums)
+        multiplier = 1
+        for i in range(len(nums)):
+            prod[i] = prod[i] * multiplier
+            multiplier = multiplier * nums[i]
+
+        multiplier = 1
+        for i in reversed(range(len(nums))):
+            prod[i] = prod[i] * multiplier
+            multiplier = multiplier * nums[i]
+        
+        return prod
