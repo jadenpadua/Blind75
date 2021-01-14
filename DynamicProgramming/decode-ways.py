b244104a9a29664fa885e8bd1a4b8ff5a661e5f5
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2021-01-14 12:09:43 -0800

    Create decode-ways.py

---
+++ DynamicProgramming/decode-ways.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        def dfs(index, ht):
+            
+            if index == len(s):
+                return 1
+            
+            if s[index] == "0":
+                return 0
+            
+            if index in ht:
+                return ht[index]
+            
+            if index + 2 <= len(s) and int(s[index: index + 2]) <= 26:
+                single = dfs(index+1, ht)
+                double = dfs(index+2, ht)
+                ht[index] = single + double
+            
+            else:
+                ht[index] = dfs(index+1, ht)
+                
+            
+            print(ht)
+            
+            return ht[index]
+        
+        
+        ht = {}
+        return dfs(0, ht)
