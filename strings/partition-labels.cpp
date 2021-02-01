701b914ff356dcb4960a7fc71862f78f810b5337
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2021-02-01 13:06:53 -0800

    Create partition-labels.cpp

---
+++ strings/partition-labels.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string S) {
+        vector<int> res;
+        vector<int> lastIdxs(26,0);
+        
+        for(int i = 0; i < S.size(); i++) {
+            lastIdxs[S[i] - 'a'] =  i;
+        }
+        
+        
+        int i = 0;
+        while(i < S.size()) {
+            int end = lastIdxs[S[i] - 'a'];
+            int j = i;
+            while(j != end) {
+                end = max(end, lastIdxs[S[j] - 'a']);
+                j++;
+            }
+            
+            res.push_back(j - i + 1);
+            i = j + 1;
+        }
+        
+        return res;
+        
+    }
+};
