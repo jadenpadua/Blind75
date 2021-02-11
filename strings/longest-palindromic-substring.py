571a7d9f922e233e235de2b7da971a72f657ed03
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2021-02-11 09:44:51 -0800

    Update longest-palindromic-substring.py

--- strings/longest-palindromic-substring.py
+++ strings/longest-palindromic-substring.py
@@ -1,22 +1,25 @@
+#abaxyzzyxf
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        #abaxyzzyxf
-        currentLongest = [0,1]
-    
-        for i in range(1,len(s)):
-            odd = self.getLongestPalindromeFrom(s,i-1,i+1)
-            even = self.getLongestPalindromeFrom(s,i-1,i)
+        longestPalindrome = [0,1]
+        
+        for i in range(1, len(s)):
+            odd = self.getLongestPalindromeFrom(s, i-1, i+1)
+            even = self.getLongestPalindromeFrom(s, i-1, i)
             
-            longest = max(odd, even, key = lambda x: x[1] - x[0])
-            currentLongest = max(longest,currentLongest,key = lambda x: x[1] - x[0])
-                 
-        return s[currentLongest[0]:currentLongest[1]]
+            currLongest = max(odd, even, key = lambda x: x[1] - x[0])
+            longestPalindrome = max(longestPalindrome, currLongest, key = lambda x: x[1] - x[0])
+        
+        return s[longestPalindrome[0] : longestPalindrome[1]]
             
-    def getLongestPalindromeFrom(self,string,left,right):
-        while left >= 0 and right < len(string):
-            if string[left] != string[right]:
+        
+    def getLongestPalindromeFrom(self, s, left, right):
+        
+        while left >= 0 and right < len(s):
+            if s[left] != s[right]:
                 break
             left -= 1
             right += 1
+            
         return [left + 1, right]
             
